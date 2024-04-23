02e16da6e2e055965087b016b6b0da81c3e6e5e3
Yahya Jabary <jabaryyahya@gmail.com>
2024-04-23 05:25:53 +0200

    up

--- src/run.py
+++ src/run.py
@@ -23,27 +23,12 @@ from scipy.stats import chi2
 # debug: assert False, f"{tokens=}"
 
 
-# - Calculate chi-square values for all unigram terms for each category
-# - Order the terms according to their value per category and preserve the top 75 terms per category
-# - Merge the lists over all categories
-
-# Produce a file output.txt from the development set that contains the following:
-# - One line for each product category (categories in alphabetic order), that contains the top 75 most discriminative terms for the category according to the chi-square test in descending order, in the following format:
+# in output.txt:
+# - One line for each product category (categories in alphabetic order), that contains the top 75 terms with highest chi2 in category in ascending order, in this format:
 # - <category name> term_1st:chi^2_value term_2nd:chi^2_value ... term_75th:chi^2_value
 # - One line containing the merged dictionary (all terms space-separated and ordered alphabetically)
 
 
-"""
-chi2 = sum((O_i - E_i)^2 / E_i)
-where:
-- O_i = observed frequency of term i in category j
-- E_i = expected frequency of term i in category j
-      = frequency of i * probability of i
-      = total frequency of term i in all categories * (total frequency of all terms in category j / total frequency of all terms in all categories)
-"""
-
-
-# TODO: Calculate chi-square values for all tokens for each category
 class ChiSquareJob(MRJob):
     def configure_args(self):
         super(ChiSquareJob, self).configure_args()
@@ -54,44 +39,62 @@ class ChiSquareJob(MRJob):
         with open(self.options.stopwords, "r") as f:
             self.stopwords = set(line.strip() for line in f)
 
-    def get_line_tokenfreq(self, _: None, line: str):
+    def get_line_termfreq(self, _: None, line: str):
         json_dict = json.loads(line)
         review_text = json_dict["reviewText"]
         category = json_dict["category"]
 
-        tokens = re.split(r'[ \t\d()\[\]{}.!?,;:+=\-_"\'~#@&*%€$§\/]+', review_text)
-        tokens = [token.lower() for token in tokens if token not in self.stopwords and len(token) > 1]
+        terms = re.split(r'[ \t\d()\[\]{}.!?,;:+=\-_"\'~#@&*%€$§\/]+', review_text)
+        terms = [t.lower() for t in terms if t not in self.stopwords and len(t) > 1]
 
-        tokenfreqs = Counter(tokens)
-        yield category, tokenfreqs
+        termfreq = Counter(terms)  # {term: freq} of a review
+        yield category, termfreq
 
-    def get_category_tokenfreq(self, category: str, tokenfreqs: list[Counter]):
-        # merge all counters
-        category_tokenfreq = Counter()
-        for tokenfreq in tokenfreqs:
-            category_tokenfreq += tokenfreq
+    def get_category_termfreq(self, category: str, termfreq: list[Counter]):
+        cat_termfreq = Counter()  # flatmap
+        for tf in termfreq:
+            cat_termfreq.update(tf)
+        yield None, {category: cat_termfreq}  # send to a single reducer
 
-        # combine
-        yield "all", {category: category_tokenfreq}
+    def get_chi2(self, _, cat_termfreqs: list[dict[str, Counter]]):
+        ctf: dict[str, Counter] = {}  # {category: {term: freq}}
+        for ct in cat_termfreqs:
+            ctf.update(ct)
 
-    def get_chi2(self, _, category_tokenfreqs: list[dict[str, Counter]]):
-        # merge all counters
-        cat_token_freqs = {}
-        for ctfs in category_tokenfreqs:
-            for category, tokenfreqs in ctfs.items():
-                cat_token_freqs[category] = tokenfreqs
+        tf: Counter = Counter()  # {term: freq} for all categories
+        for termfreq in ctf.values():
+            tf.update(termfreq)
 
-        # calculate total frequency of all terms in all categories
-        total_freq_all_terms_all_categories = sum(sum(tokenfreq.values()) for tokenfreq in cat_token_freqs.values())
-        yield "all", total_freq_all_terms_all_categories
+        # 1) calculate chi2 of all terms for each category
+        chi2_values = {}  # {category: {term: chi2}}
+        for cat, termfreq in ctf.items():
+            chi2_values[cat] = {}
+            total_in_cat = sum(termfreq.values())
+            total_in_all = sum(tf.values())
+
+            for term, freq in termfreq.items():
+                observed = freq
+                expected = tf[term] * total_in_cat / total_in_all
+                chi2_values[cat][term] = (observed - expected) ** 2 / expected
+
+        # 2) only keep top 75 terms per category with highest chi2 values, sort terms based on chi2 values
+        top_terms = {}
+        for cat, term_chi2 in chi2_values.items():
+            top_terms[cat] = heapq.nlargest(75, term_chi2, key=term_chi2.get)
+
+        # 3) merge all terms, over all categories
+        all_terms = sorted(set(itertools.chain.from_iterable(top_terms.values())))
+        all_terms_str = " ".join(all_terms)
+
+        yield None, {"top_terms": top_terms, "all_terms": all_terms_str}
 
     def steps(self):
         # fmt: off
         return [
             MRStep(
                 mapper_init=self.init,
-                mapper=self.get_line_tokenfreq,
-                reducer=self.get_category_tokenfreq
+                mapper=self.get_line_termfreq,
+                reducer=self.get_category_termfreq
             ),
             MRStep(
                 reducer=self.get_chi2
