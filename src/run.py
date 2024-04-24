cdf76b22d109df4744372b3bdb60ca7fc9530ac3
Yahya Jabary <jabaryyahya@gmail.com>
2024-04-24 19:13:43 +0200

    up

--- src/run.py
+++ src/run.py
@@ -38,66 +38,60 @@ class ChiSquareJob(MRJob):
         terms = set(terms) - self.stopwords
         terms = [t.lower() for t in terms if len(t) > 1]
 
-        # channel for category count
-        yield ("__chan__", category), 1
-
-        # channel for term count (a line has multiple terms, so we can't use a single yield)
         for term in terms:
             yield (term, category), 1
 
-    def combiner(self, key: str | tuple[str, str], counts: list[int]):
-        # avoid channel key conflict
-        # if isinstance(key, tuple):
-        #     assert key[0] != "__chan__"
-        # else:
-        #     assert key == "__chan__"
+        # each line / review has exactly one category
+        yield (None, category), 1
 
-        # chunk multiple yields from the same mapper, send all to the same reducer
+    def combiner(self, key: tuple[None | str, str], counts: list[int]):
+        # aggregate counts from both channels, send to single reducer
         yield None, (key, sum(counts))
 
-    def reducer(self, _: None, vals: list[tuple[str | tuple[str, str], int]]):
+    def reducer(self, _: None, key_count: list[tuple[tuple[None | str, str], int]]):
         N = 0
-        term_count = defaultdict(int)
         cat_count = defaultdict(int)
+        term_count = defaultdict(int)
         term_cat_count = defaultdict(int)
 
-        for elem in vals:
-            assert len(elem) == 2
-            key: str | tuple[str, str] = elem[0]
-            val: int = elem[1]
-
-            if isinstance(key, str):
-                N += val
+        for key, count in key_count:
+            term, cat = key
+            if term == None:
+                N += count
+                cat_count[cat] = count
             else:
-                term, category = key
-                term_cat_count[(term, category)] += val
-                term_count[term] += val
-                cat_count[category] += val
+                term_count[term] += count
+                term_cat_count[(term, cat)] = count
 
+        # 1) calculate chi2 of all terms for each category
+        # can't merge with previous loop because we need to calculate N first
         chi2_cat_term = {}
-        for term, category in term_cat_count:
-            A = term_cat_count[(term, category)]
+        for term, cat in term_cat_count:
+            A = term_cat_count[(term, cat)]
             B = term_count[term] - A
-            C = cat_count[category] - A
+            C = cat_count[cat] - A
             D = N - A + B + C
 
             chi2 = (N * (A * D - B * C) ** 2) / ((A + B) * (A + C) * (B + D) * (C + D))
-            if category not in chi2_cat_term:
-                chi2_cat_term[category] = {}
-            chi2_cat_term[category][term] = chi2
+            if cat not in chi2_cat_term:
+                chi2_cat_term[cat] = {}
+            chi2_cat_term[cat][term] = chi2
 
-        # sort, get top 75
+        # 2) sort chi2 keys alphabetically
+        chi2_cat_term = dict(sorted(chi2_cat_term.items(), key=lambda x: x[0]))
+
+        # 3) sort chi2 values and get top 75
         for cat, terms in chi2_cat_term.items():
             chi2_cat_term[cat] = dict(heapq.nlargest(75, terms.items(), key=lambda x: x[1]))
-
             if not chi2_cat_term[cat]:
                 del chi2_cat_term[cat]
 
-        # sort by category
-        chi2_cat_term = dict(sorted(chi2_cat_term.items(), key=lambda x: x[0]))
-
+        # 4) yield results
+        # <category name> term1:chi2 term2:chi2 ... term75:chi2
         for cat, terms in chi2_cat_term.items():
             yield None, str(cat) + " " + " ".join(f"{term}:{chi2}" for term, chi2 in terms.items())
+        # all terms space-separated and ordered alphabetically
+        yield None, " ".join(sorted(list(itertools.chain.from_iterable(chi2_cat_term.values()))))
 
     def steps(self):
         # fmt: off
